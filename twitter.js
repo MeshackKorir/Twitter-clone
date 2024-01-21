4fed31065b0d3fa7227c8aa3bd4770153f947ebe
MeshackKorir <meshackkorir100@gmail.com>
2024-01-21 20:39:27 +0300

    function fetch comments

--- twitter.js
+++ twitter.js
@@ -102,3 +102,18 @@ window.onclick = function (event) {
         productDiv.appendChild(postElement);
     })
  }
+
+
+
+ // Function to fetch comments
+
+ async function fetchComments(postId) {
+    try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+        const comments = await response.json();
+        return comments;
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+        return [];
+    }
+ }
